596f256bd2d1496cdf35927f56dec1a6738f8830
RudraDeshmukh <99274112+RudraDeshmukh@users.noreply.github.com>
2022-06-21 21:08:25 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+
+difference=0;
+leftwristX=0;
+rightwristX=0;
+
+function setup(){
+    canvas = createCanvas(500,500)
+    canvas.position(560,100)
+
+    Video = createCapture(VIDEO)
+    Video.size(500,550)
+
+    poseNet = ml5.poseNet(Video,modelloaded)
+    poseNet.on('pose',gotPoses)
+
+}
+
+function draw(){
+    background("#637187")
+    fill("#eb59ba")
+    stroke("#eb59ba")
+    textSize(difference)
+    text("Rudra",200,200)
+}
+
+function modelloaded(){
+    console.log("PoseNet is initialized")
+}
+
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results);
+        leftwristX=results[0].pose.leftWrist.x
+        rightwristX=results[0].pose.rightWrist.x
+        difference = floor(leftwristX - rightwristX)
+        console.log(leftwristX,rightwristX)
+        document.getElementById("square_side").innerHTML="The width and height of the text are " + difference + "px"
+
+
+    }
+}
